0f66ab36ede735734ea6c9b651acaed400bc2085
Showmee <milosz.wadas@gmail.com>
2014-10-18 17:01:27 +0200

    fix, works now

--- JSF_JSON_Reader/src/core/JSONReaderBean.java
+++ JSF_JSON_Reader/src/core/JSONReaderBean.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-
 import java.util.Iterator;
 
 import model.Customer;
@@ -19,7 +18,6 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-
 @ManagedBean(name = "Jbean")
 @SessionScoped
 /**
@@ -31,11 +29,10 @@ public class JSONReaderBean implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
-	//array to store the result
+
+	// array to store the result
 	public ArrayList<Customer> list = new ArrayList<Customer>();
-	
+
 	public ArrayList<Customer> getList() {
 		return list;
 	}
@@ -45,45 +42,63 @@ public class JSONReaderBean implements Serializable {
 	}
 
 	public void init() {
-		
-		JSONParser parser = new JSONParser();
-		
 		try {
-			Object obj = parser.parse(new FileReader("c:\\file.json"));
-			System.out.println("File found!");
-			
-			Customer c = new Customer();
+			JSONParser parser = new JSONParser();
+			Object obj = parser.parse(new FileReader("c:\\test.json"));
 			JSONObject jsonObject = (JSONObject) obj;
+
+			System.out.println("File found!");
+
+			Customer c = new Customer();
+
 			ArrayList<String> temp = new ArrayList<String>();
-			
+
 			long id = (Long) jsonObject.get("id");
 			System.out.println(id);
-			
+
 			// loop array
+			//JSONArray names = (JSONArray) jsonObject.get("name");
+
+			/*
+			 * Iterator<String> iterator = names.iterator(); while
+			 * (iterator.hasNext()) { System.out.println(iterator.next());
+			 * temp.add(iterator.next()); }
+			 */
+
+			// get an array from the JSON object
 			JSONArray names = (JSONArray) jsonObject.get("name");
-			
-			Iterator<String> iterator = names.iterator();
-			while (iterator.hasNext()) {
-				System.out.println(iterator.next());
-				temp.add(iterator.next());
+
+			// take the elements of the json array
+			for (int i = 0; i < names.size(); i++) {
+				System.out.println("The " + i + " element of the array: "
+						+ names.get(i));
+				temp.add(""+names.get(i));
 			}
-			
+
+			/*// take each value from the json array separately
+			Iterator i = names.iterator();
+			while (i.hasNext()) {
+				JSONObject innerObj = (JSONObject) i.next();
+				System.out.println("language " + innerObj.get("lang");
+						//+ " with level " + innerObj.get("knowledge"));
+			}*/
+
 			String surname = (String) jsonObject.get("surname");
 			System.out.println(surname);
-			
+
 			long salary = (Long) jsonObject.get("salary");
 			System.out.println(salary);
-			
-			c.setId((int)id);
+
+			c.setId((int) id);
 			c.setNames(temp);
 			c.setSurname(surname);
-			c.setSalary((int)salary);
-			
+			c.setSalary((int) salary);
+
 			list.add(c);
-			
-			//System.out.println(c.toString());
+
+			// System.out.println(c.toString());
 			setList(list);
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -91,11 +106,6 @@ public class JSONReaderBean implements Serializable {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
-	
-	
-	
+
 }
