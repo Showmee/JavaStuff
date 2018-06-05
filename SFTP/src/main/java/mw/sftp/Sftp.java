696b362eafe03450557fd22fd6218e11a0125c43
milooW <milosz.wadas@gmail.com>
2018-06-05 23:42:44 +0200

    annotations

--- SFTP/src/main/java/mw/sftp/Sftp.java
+++ SFTP/src/main/java/mw/sftp/Sftp.java
@@ -16,44 +16,53 @@ import com.jcraft.jsch.SftpException;
 
 import mw.cfg.Config;
 
+/**
+ * Module responsible for basic SFTP operations
+ * 
+ * @author Milosz Wadas
+ * @version 0.1
+ * @since 2018-06-01
+ *
+ */
 public class Sftp
 {
-
-	public Sftp() {
-		
-	}
 	
+	private final String STATUS_OK = "OK";
+
+	public Sftp()
+	{
+
+	}
+
 	public String execute(String command, File f)
 	{
 
-		if ( f == null )
+		if (f == null)
 			System.out.println("File not found");
-		
+
 		String PARAM_CMD_PUT = "PUT";
 		String PARAM_HOSTNAME = "127.0.0.1";
 		int PARAM_PORT = 22;
 		String PARAM_USER = "abc";
 		String PARAM_PASSWORD = "qqq";
 		String PARAM_RESULT = "OK";
-		
+
 		JSch jsch = new JSch();
-		
-		
+
 		Session session = prepareSession(PARAM_USER, PARAM_PASSWORD, PARAM_HOSTNAME, PARAM_PORT, jsch);
-		
+
 		if (PARAM_CMD_PUT.equals(command))
 		{
-			//String fileExt = FilenameUtils.getExtension(f.getName());
-			//String fileName = f.getName()+"."+fileExt;
-			
-			sendFile(session, f.getName());//, PARAM_DIR);
+			// String fileExt = FilenameUtils.getExtension(f.getName());
+			// String fileName = f.getName()+"."+fileExt;
+
+			sendFile(session, f.getName());// , PARAM_DIR);
 		}
 		else
 		{
 			throw new IllegalArgumentException();
 		}
-		
-		
+
 		return PARAM_RESULT;
 	}
 
@@ -62,20 +71,20 @@ public class Sftp
 		try
 		{
 			java.util.Properties config = new java.util.Properties();
-	        config.put("StrictHostKeyChecking", "no");
-			
+			config.put("StrictHostKeyChecking", "no");
+
 			Session s = jsch.getSession(user, host, port);
 			s.setConfig(config);
 			s.setPassword(pass);
 			s.connect();
-			
+
 			return s;
-		} 
+		}
 		catch (JSchException e)
 		{
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 
@@ -85,35 +94,42 @@ public class Sftp
 		{
 			Channel channel = session.openChannel("sftp");
 			channel.connect();
-			
+
 			ChannelSftp c = (ChannelSftp) channel;
-			
-			return c;	
+
+			return c;
 		}
 		else
 			throw new JSchException();
-		
+
 	}
-	
-	private String sendFile(final Session session, final String filePath)//, final String remotePath) 
+
+	/**
+	 * Uploads file to sftp
+	 * 
+	 * @param session
+	 * @param filePath
+	 * @return
+	 */
+
+	private String sendFile(final Session session, final String filePath)// , final String remotePath)
 	{
 		try
 		{
 			ChannelSftp c = prepareConnection(session);
-			
+
 			c.put(filePath, filePath);
-		} catch (JSchException e)
+		}
+		catch (JSchException e)
 		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SftpException e)
-		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return "";
+		catch (SftpException e)
+		{
+			e.printStackTrace();
+		}
+
+		return STATUS_OK;
 	}
 
-	
 }
